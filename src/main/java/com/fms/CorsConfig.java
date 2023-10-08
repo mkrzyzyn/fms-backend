fe3ac67ffbb7667409e741e08a5c1fa300bf767e
mkrzyzyn <maciej.krzyzynski@gmail.com>
2023-10-08 17:32:49 +0200

    Working commit. CORS.

--- src/main/java/com/fms/CorsConfig.java
+++ src/main/java/com/fms/CorsConfig.java
@@ -4,21 +4,48 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
 
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                System.out.println("CORS configuration is being applied.");
+//                registry.addMapping("/api/offers")
+//                        .allowedOrigins("http://localhost:8000") // Allow requests from this origin
+//                        .allowedMethods("GET", "POST", "PUT", "DELETE") // Allow these HTTP methods
+//                        .allowedHeaders("*"); // Allow all headers
+//
+//            }
+//        };
+//    }
+
     @Bean
     public CorsFilter corsFilter() {
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.addAllowedOrigin("*"); // Allow requests from any origin
-        corsConfiguration.addAllowedMethod("GET"); // Allow only GET requests
-        corsConfiguration.addAllowedHeader("*"); // Allow all headers
-
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", corsConfiguration);
+        CorsConfiguration config = new CorsConfiguration();
 
+        // Allow requests from specific origins
+        config.addAllowedOrigin("http://localhost:8000");
+
+        // Allow specific HTTP methods (e.g., GET, POST, PUT, DELETE)
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("DELETE");
+        config.addAllowedMethod("OPTIONS");
+
+        // Allow specific headers if needed
+        config.addAllowedHeader("Authorization");
+
+        source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
     }
+
 }
 
--- src/main/java/com/fms/controller/LoanOffersController.java
+++ src/main/java/com/fms/controller/LoanOffersController.java
@@ -4,6 +4,7 @@ import com.fms.model.LoanOffersEntity;
 import com.fms.repository.LoanOffersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/offers")
+@CrossOrigin(origins = "http://localhost:8000")
 public class LoanOffersController{
         private final LoanOffersRepository repository;
 
